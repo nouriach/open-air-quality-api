ec2d95bf478be65cb7097234d46fba804d3be9ba
nouriach <49981579+nouriach@users.noreply.github.com>
2021-06-16 15:13:49 +0100

    Create API Service and update startup

---
+++ api-air-quality.Web/Application/Common/Interfaces/IApiService.cs
@@ -0,0 +1,13 @@
+﻿using api_air_quality.Web.Application.Services.Countries.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_air_quality.Web.Application.Common
+{
+    public interface IApiService
+    {
+        Task<Domain.Models.Rootobject> GetAllCountriesAsync(GetAllCountriesQuery query);
+    }
+}
