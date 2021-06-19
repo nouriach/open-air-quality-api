4359084c36de8d5e351046ea631b650db9ad7969
nouriach <49981579+nouriach@users.noreply.github.com>
2021-06-19 16:25:17 +0100

    Create Validation behaviour for GetCities query

---
+++ api-air-quality.Web/Application/Services/Cities/Validators/GetCitiesByCountryQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using api_air_quality.Web.Application.Services.Cities.Queries;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_air_quality.Web.Application.Services.Cities.Validators
+{
+    public class GetCitiesByCountryQueryValidator : AbstractValidator<GetCitiesByCountryQuery>
+    {
+        public GetCitiesByCountryQueryValidator()
+        {
+            RuleFor(q => q.CountryCode).NotEmpty().Length(2,2);
+        }
+    }
+}
---
+++ api-air-quality.Web/Application/Services/Countries/Validators/GetAllCountriesQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using api_air_quality.Web.Application.Services.Countries.Queries;
+using FluentValidation;
+
+namespace api_air_quality.Web.Application.Services.Countries.Validators
+{
+    public class GetAllCountriesQueryValidator : AbstractValidator<GetAllCountriesQuery>
+    {
+        public GetAllCountriesQueryValidator()
+        {
+            //
+        }
+    }
+}
